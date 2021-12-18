a6e43ded94a7efa2cc568bea89d9fc81da11b5db
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-12-18 22:57:55 +0700

    Add files via upload

---
+++ untuk github.cpp
@@ -0,0 +1,26 @@
+#include <ncurses\ncurses.h>
+#include <windows.h>
+using namespace std;
+
+int main(){
+	
+	initscr();
+	
+	for (int i=0;i<=3;i++){
+		mvprintw(15,35,"Loading");
+		mvprintw(15,42,".");
+		refresh();
+		Sleep(300);
+		mvprintw(15,43,".");
+		refresh();
+		Sleep(300);
+		mvprintw(15,44,".");
+		refresh();
+		Sleep(300);
+		
+		clear();	
+	}
+	
+	getch();
+	endwin();
+}
