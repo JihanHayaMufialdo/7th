1c41f5ca4f6a2cb46fba78911db1ff91d6adbe23
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-12-18 22:58:22 +0700

    Update untuk github.cpp

--- untuk github.cpp
+++ untuk github.cpp
@@ -4,23 +4,4 @@ using namespace std;
 
 int main(){
 	
-	initscr();
-	
-	for (int i=0;i<=3;i++){
-		mvprintw(15,35,"Loading");
-		mvprintw(15,42,".");
-		refresh();
-		Sleep(300);
-		mvprintw(15,43,".");
-		refresh();
-		Sleep(300);
-		mvprintw(15,44,".");
-		refresh();
-		Sleep(300);
-		
-		clear();	
-	}
-	
-	getch();
-	endwin();
 }
